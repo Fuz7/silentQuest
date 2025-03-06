429d08355fa04f9f14d34633507b55e02aa09d26
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2025-03-06 12:12:02 +0800

    Refactor meditation controller to use meditation repository

--- app/Http/Controllers/MeditationController.php
+++ app/Http/Controllers/MeditationController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Meditation;
+use App\Repository\MeditationRepository;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MeditationController extends Controller
 {
+
+  protected $meditationRepository;
+
+  public function __construct() {
+    $this->meditationRepository = new MeditationRepository;
+  }
+
     //
     public function storeOrUpdate(Request $request){
         
@@ -17,61 +25,21 @@ class MeditationController extends Controller
         ]);
         
         $user_id =  Auth::user()->id;
-        $meditatedToday =  Meditation::where('user_id',$user_id)
-        ->whereDate('created_at',Carbon::today());
+        $this->meditationRepository->storeOrUpdate($request['duration'],$user_id);
 
-        if($meditatedToday->exists()){
-          $previousDurationString = $meditatedToday->pluck('duration')[0];
-          $currentDurationInteger = $request->input('duration');
-          $totalTime = Carbon::createFromFormat('H:i:s', $previousDurationString)->addSeconds($currentDurationInteger);
-          $formattedTime = $totalTime->format('H:i:s');
-          $meditatedToday->update(['duration' =>$formattedTime]);
-        }else{
-          $seconds = $request->input('duration');
-          $time = Carbon::createFromTimestamp($seconds);
-          Meditation::create([
-              'user_id' =>$user_id,
-              'duration' => $time,
-          ]);
-      }
-
-        return;
 
     }
 
     public static function getTotalMeditationTime(){
-        $user = Auth::user();
-        $meditations = Meditation::where('user_id',$user['id'])
-                                 ->get();
-        $date = Carbon::now()->startOfDay();  // Outputs: 2024-01-24 00:00:00
-
-        foreach ($meditations as $meditation) {
-          # code...
-          $meditationTime = abs(Carbon::createFromFormat('H:i:s',$meditation['duration'])
-                                    ->diffInSeconds(Carbon::today()->startOfDay()));
-          $date->addSeconds($meditationTime);
-
-        }
-        $formattedTime = $date->format('H\h i\m s\s');
-        return($formattedTime);
+        $user_id = Auth::user()->id;
+        $response = MeditationRepository::getTotalMeditationTime($user_id);
+        return($response);
     }
     public static function getTotalMeditationTimeToday(){
-      $user = Auth::user();
-      $meditation = Meditation::where('user_id',$user['id'])
-                                  ->whereDate('created_at',Carbon::today())
-                                  ->first();
-      $meditationTime = $meditation['duration'] ?? "00:00:00";
-      $dateFormat = Carbon::createFromFormat('H:i:s',$meditationTime);
-      $parts = explode(':', $dateFormat);
-      $hour = (int)substr($parts[0],-2); 
-      $formattedTime ='';
-      if($hour > 0){
-        $formattedTime = $dateFormat->format('G:i:s');
-      }else{
-        $formattedTime = $dateFormat->format('i:s');
-      }
+      $user_id = Auth::user()->id;
+      $response = MeditationRepository::getTotalMeditationTimeToday($user_id);
  
-      return($formattedTime);
+      return($response);
     }
 
 }
